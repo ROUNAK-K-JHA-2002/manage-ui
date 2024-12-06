07fb91c724f746149611cc613935b6558294df36
ROUNAK-K-JHA-2002 <rounakjha291202.rar@gmail.com>
2024-12-06 21:40:58 +0530

    Complete

--- src/app/Components/Footer/index.tsx
+++ src/app/Components/Footer/index.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { navbarMenus } from "@/app/utils/constants";
 import {
   iconFacebook,
@@ -8,10 +9,12 @@ import {
   logo,
 } from "@/assets";
 import Image from "next/image";
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import PrimaryButton from "../Buttons/PrimaryButton";
 
 const Footer: FC = () => {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState<any>(null);
   return (
     <div className=" bg-neutral-veryDarkBlue w-screen md:h-60 z-10 flex flex-col-reverse md:flex-row text-white py-6 px-4">
       <div className=" w-full md:w-1/4 h-full flex flex-col-reverse md:flex-col items-center justify-evenly gap-8">
@@ -33,7 +36,7 @@ const Footer: FC = () => {
       </div>
       <div className=" flex  w-full  justify-around py-5 px-3  mb-6">
         {" "}
-        <div className=" w-full md:w-1/5 h-full flex  justify-center gap-4 flex-col">
+        <div className=" w-full md:w-1/5 h-full flex  justify-center gap-4 flex-col hover:text-neutral-darkGrayishBlue cursor-pointer">
           {navbarMenus.map((item) => (
             <div key={item}>{item}</div>
           ))}
@@ -46,12 +49,36 @@ const Footer: FC = () => {
       </div>
       <div className=" w-full md:w-1/4 h-full flex flex-col justify-evenly">
         <div className=" flex gap-3 ">
-          <input
-            type="email"
-            className=" focus:outline-none rounded-2xl px-3"
-            placeholder="Updates in your Inbox..."
-          />
-          <div className=" bg-primary-brightRed px-6 py-2 rounded-3xl">Go</div>
+          <div>
+            <input
+              type="email"
+              name="email"
+              required
+              value={email}
+              onChange={(e) => {
+                setError(null);
+                setEmail(e.target.value);
+              }}
+              className=" focus:outline-none rounded-2xl px-3 py-2 text-black"
+              placeholder="Updates in your Inbox..."
+            />
+            {error !== null && <p className=" text-red-500 text-sm mt-2">{error}</p>}
+          </div>
+          <div
+            className=" bg-primary-brightRed px-6 py-2 rounded-3xl cursor-pointer h-max"
+            onClick={() => {
+              if (email.trim().length === 0) {
+                setError("Email Cannot be empty!");
+                return;
+              }
+              if (!email.includes("@") || !email.includes(".")) {
+                setError("Invalid Email format");
+                return;
+              }
+            }}
+          >
+            Go
+          </div>
         </div>
         <div className=" text-neutral-darkGrayishBlue text-sm md:block hidden">
           Copyright {new Date().getFullYear()}.All Right Reserved.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import FeaturesPage from "./Components/FeaturesPage";
 import Testimonials from "./Components/Testimonials";
 import Banner from "./Components/Banner";
 import Footer from "./Components/Footer";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -21,6 +22,10 @@ export default function Home() {
       <Testimonials />
       <Banner />
       <Footer />
+      <div className=" bg-neutral-veryDarkBlue flex flex-col items-center justify-center gap-3 text-white border-1 border-white/30 py-4">
+       <p> Challenge by <Link href={"https://www.frontendmentor.io/challenges/manage-landing-page-SLXqC6P5"} target="blank">Frontend Mentor</Link></p>
+        v06.12.2024
+      </div>
     </div>
   );
 }
